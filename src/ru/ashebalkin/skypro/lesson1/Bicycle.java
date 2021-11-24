b655d5039e7a6ed15219290935ddd8d2eae3b666
antoshka <ashebalkin@diasoft.ru>
2021-11-25 00:08:04 +0300

    Создание ветки для сдачи 1-го ДЗ 2-го курса

--- src/ru/ashebalkin/skypro/lesson1/Bicycle.java
+++ src/ru/ashebalkin/skypro/lesson1/Bicycle.java
@@ -1,2 +1,11 @@
-package ru.ashebalkin.skypro.lesson1;public class Bicycle {
+package ru.ashebalkin.skypro.lesson1;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
 }
--- src/ru/ashebalkin/skypro/lesson1/Truck.java
+++ src/ru/ashebalkin/skypro/lesson1/Truck.java
@@ -1,2 +1,19 @@
-package ru.ashebalkin.skypro.lesson1;public class Truck {
+package ru.ashebalkin.skypro.lesson1;
+
+public class Truck {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
 }
